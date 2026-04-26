c2a10cd174b6ba5e08c327aaaf04fbf36837bb2a
Veronika050987 <veronicaviv@mail.ru>
2026-04-26 14:16:16 +0300

    pagination DONE

--- RazorPages/Pages/Students/Index.cshtml.cs
+++ RazorPages/Pages/Students/Index.cshtml.cs
@@ -9,30 +9,41 @@ using Microsoft.EntityFrameworkCore;
 using RazorPages.Data;
 using RazorPages.Models;
 
+using Microsoft.Extensions.Configuration;
+
 namespace RazorPages.Pages.Students
 {
     public class IndexModel : PageModel
     {
         private readonly RazorPages.Data.ContosoUniversityContext _context;
 
-        public IndexModel(RazorPages.Data.ContosoUniversityContext context)
+        public IndexModel(RazorPages.Data.ContosoUniversityContext context, IConfiguration configuration)
         {
             _context = context;
+            this.configuration = configuration;
         }
-
+        //Search & Sorting
         public string NameSort { get; set; }
         public string DateSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Students { get;set; } = default!;
+        //public IList<Student> Students { get;set; } = default!;
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+
+        //Pagination:
+        readonly IConfiguration configuration;
+        public PaginatedList<Student> Students { get; set; }
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null) pageIndex = 1;
+            else searchString = currentFilter;
             CurrentFilter = searchString;
-            
+
             IQueryable<Student> students = from student in _context.Students select student;
             
             if(!String.IsNullOrEmpty(CurrentFilter))
@@ -48,32 +59,34 @@ namespace RazorPages.Pages.Students
                 default:            students = students.OrderBy(s => s.ID);                         break;
             }
 
-            Students = await students.AsNoTracking().ToArrayAsync();
+            int pageSize = configuration.GetValue("PageSize", 10);
+            Students = await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), pageIndex ?? 1, pageSize);
+            //Students = await students.AsNoTracking().ToArrayAsync();
             //Students = await _context.Students.ToListAsync();
         }
-        public async Task OnPostAsync(string sortOrder, string searchString)
-        {
-            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
-            CurrentFilter = searchString;
+        //public async Task OnPostAsync(string sortOrder, string searchString)
+        //{
+        //    NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+        //    DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+        //    CurrentFilter = searchString;
             
-            IQueryable<Student> students = from student in _context.Students select student;
+        //    IQueryable<Student> students = from student in _context.Students select student;
             
-            if(!String.IsNullOrEmpty(CurrentFilter))
-            {
-                students = students.Where(s => s.LastName.Contains(CurrentFilter) || s.FirstName.Contains(CurrentFilter));
-            }
+        //    if(!String.IsNullOrEmpty(CurrentFilter))
+        //    {
+        //        students = students.Where(s => s.LastName.Contains(CurrentFilter) || s.FirstName.Contains(CurrentFilter));
+        //    }
 
-            switch (sortOrder)
-            {
-                case "name_desc":   students = students.OrderByDescending(s => s.LastName);         break;
-                case "date_desc":   students = students.OrderByDescending(s => s.EnrollmentDate);   break;
-                case "Date":        students = students.OrderBy(s => s.EnrollmentDate);             break;
-                default:            students = students.OrderBy(s => s.ID);                         break;
-            }
+        //    switch (sortOrder)
+        //    {
+        //        case "name_desc":   students = students.OrderByDescending(s => s.LastName);         break;
+        //        case "date_desc":   students = students.OrderByDescending(s => s.EnrollmentDate);   break;
+        //        case "Date":        students = students.OrderBy(s => s.EnrollmentDate);             break;
+        //        default:            students = students.OrderBy(s => s.ID);                         break;
+        //    }
 
-            Students = await students.AsNoTracking().ToArrayAsync();
-            //Students = await _context.Students.ToListAsync();
-        }
+        //    Students = await students.AsNoTracking().ToArrayAsync();
+        //    //Students = await _context.Students.ToListAsync();
+        //}
     }
 }
