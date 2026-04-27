96a532cc9400e1548aadc586471ac2b5bd63ffa6
Veronika050987 <veronicaviv@mail.ru>
2026-04-27 15:32:25 +0300

    data annotations in student model done

--- RazorPages/Models/Student.cs
+++ RazorPages/Models/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPages.Models
 {
@@ -6,9 +7,30 @@ namespace RazorPages.Models
 	{
 		public int ID { get; set; }
 
+		[Required]
+		[StringLength(50, ErrorMessage = "Фамилия может быть не более 50 символов")]
+		[Display(Name = "Фамилия")]
+		[RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Строка содержит недопустимые символы")]
 		public string LastName { get; set; }
+		
+		[Required(ErrorMessage = "Студент без имени - не студент")]
+		[StringLength(50, ErrorMessage = "Имя может быть не более 50 символов")]
+		[Display(Name = "Имя")]
+		[RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Строка содержит недопустимые символы")]
+		
 		public string FirstName { get; set; }
+
+		[Display(Name = "Дата зачисления")]
+		[DataType(DataType.Date)]
+		//[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+		//[DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
 		public DateTime EnrollmentDate { get; set; }
+
+		[Display(Name = "Студент")]
+		public string FullName
+		{
+			get => $"{LastName} {FirstName}";
+		}
 
 		//Navigation properties
 		public ICollection<Enrollment> Enrollments { get; set; }
