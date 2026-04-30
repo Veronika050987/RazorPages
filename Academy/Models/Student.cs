52fc3ecb460847728e5fbb4e4344b4dc8a476a98
Veronika050987 <veronicaviv@mail.ru>
2026-04-30 20:41:56 +0300

    Index students with colors DONE

--- Academy/Models/Student.cs
+++ Academy/Models/Student.cs
@@ -8,16 +8,29 @@ namespace Academy.Models
 	{
 		[Key]
 		public int stud_id { get; set; }
-		[Required]
+
+		[Required(ErrorMessage = "Без фамилии не бывает никого, даже 🐱!")]
+		[StringLength(15, ErrorMessage = "Фамилия не может превышать 15 символов")]
+		[Display(Name = "Фамилия")]
+		[RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Строка содержит недопустимые символы")]
 		public string last_name { get; set; }
-		[Required]
+
+		[Required(ErrorMessage = "Потому что имя должно быть даже у 🦔")]
+		[StringLength(15, ErrorMessage = "Имя не может превышать 15 символов")]
+		[Display(Name = "Имя")]
+		[RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Строка содержит недопустимые символы")]
 		public string first_name { get; set; }
+		
+		[Display(Name = "Отчество")]
 		public string? middle_name { get; set; }
-		[Required, DataType(DataType.Date)]
+
+		[Required(ErrorMessage = "Только 🧟 не имеют дату рождения")]
+		[DataType(DataType.Date)]
+		[Display(Name = "Дата рождения")]
 		public DateOnly birth_date { get; set; }
-		public string? email { get; set; }
-		public string? phone { get; set; }
-		public byte[]? photo { get; set; }
+		//public string? email { get; set; }
+		//public string? phone { get; set; }
+		//public byte[]? photo { get; set; }
 		//[Required, ForeignKey(nameof(Group))]
 		//public int group { get; set; }
 		//Navigation properties
